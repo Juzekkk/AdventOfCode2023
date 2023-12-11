0dafcc162614b988c2589769ff3223446bc9dd7c
Juzekkk <juzekgaming@gmail.com>
2023-12-11 16:26:34 +0100

    Day 11 Solution Part 2

--- 11-Cosmic_Expansion/src/main.cpp
+++ 11-Cosmic_Expansion/src/main.cpp
@@ -11,11 +11,15 @@
 using Grid = std::vector<std::vector<char>>;
 using Position = std::pair<int, int>;
 
-std::vector<Position> findHashPositions(const Grid& grid) {
+std::vector<Position> findHashPositions(const Grid &grid)
+{
     std::vector<Position> positions;
-    for (int i = 0; i < grid.size(); ++i) {
-        for (int j = 0; j < grid[i].size(); ++j) {
-            if (grid[i][j] == '#') {
+    for (int i = 0; i < grid.size(); ++i)
+    {
+        for (int j = 0; j < grid[i].size(); ++j)
+        {
+            if (grid[i][j] == '#')
+            {
                 positions.emplace_back(i, j);
             }
         }
@@ -23,31 +27,37 @@ std::vector<Position> findHashPositions(const Grid& grid) {
     return positions;
 }
 
-long long calculateModifiedManhattanDistance(const Position& a, const Position& b, 
-                                       const std::vector<size_t>& emptyRows, 
-                                       const std::vector<size_t>& emptyCols,
-                                       long long factor) {
+long long calculateModifiedManhattanDistance(const Position &a, const Position &b,
+                                             const std::vector<size_t> &emptyRows,
+                                             const std::vector<size_t> &emptyCols,
+                                             long long factor)
+{
     long long distance = 0;
 
-    for (int i = std::min(a.first, b.first); i <= std::max(a.first, b.first); ++i) {
+    for (int i = std::min(a.first, b.first); i <= std::max(a.first, b.first); ++i)
+    {
         distance += (std::find(emptyRows.begin(), emptyRows.end(), i) != emptyRows.end()) ? factor : 1;
     }
 
-    for (int j = std::min(a.second, b.second); j <= std::max(a.second, b.second); ++j) {
+    for (int j = std::min(a.second, b.second); j <= std::max(a.second, b.second); ++j)
+    {
         distance += (std::find(emptyCols.begin(), emptyCols.end(), j) != emptyCols.end()) ? factor : 1;
     }
 
     return distance - 2;
 }
 
-std::vector<long long> findAllPairwiseDistances(const std::vector<Position>& positions,
-                                          const std::vector<size_t>& emptyRows,
-                                          const std::vector<size_t>& emptyCols,
-                                          long long factor) {
+std::vector<long long> findAllPairwiseDistances(const std::vector<Position> &positions,
+                                                const std::vector<size_t> &emptyRows,
+                                                const std::vector<size_t> &emptyCols,
+                                                long long factor)
+{
     std::vector<long long> allDistances;
 
-    for (size_t i = 0; i < positions.size(); ++i) {
-        for (size_t j = i + 1; j < positions.size(); ++j) {
+    for (size_t i = 0; i < positions.size(); ++i)
+    {
+        for (size_t j = i + 1; j < positions.size(); ++j)
+        {
             long long distance = calculateModifiedManhattanDistance(positions[i], positions[j], emptyRows, emptyCols, factor);
             allDistances.push_back(distance);
         }
@@ -56,8 +66,8 @@ std::vector<long long> findAllPairwiseDistances(const std::vector<Position>& pos
     return allDistances;
 }
 
-
-Grid readGridFromFile(const std::string &filePath) {
+Grid readGridFromFile(const std::string &filePath)
+{
     Grid grid;
     std::ifstream file(filePath);
     std::string line;
@@ -68,34 +78,69 @@ Grid readGridFromFile(const std::string &filePath) {
     return grid;
 }
 
-std::vector<size_t> emptyRows(const Grid& grid) {
+std::vector<size_t> emptyRows(const Grid &grid)
+{
     std::vector<size_t> emptyRows;
-    for (size_t i = 0; i < grid.size(); ++i) {
-        if (std::none_of(grid[i].begin(), grid[i].end(), [](char c) { return c == '#'; })) {
+    for (size_t i = 0; i < grid.size(); ++i)
+    {
+        if (std::none_of(grid[i].begin(), grid[i].end(), [](char c)
+                         { return c == '#'; }))
+        {
             emptyRows.push_back(i);
         }
     }
     return emptyRows;
 }
 
-std::vector<size_t> emptyColumns(const Grid& grid) {
+std::vector<size_t> emptyColumns(const Grid &grid)
+{
     std::vector<size_t> emptyCols;
 
     size_t colCount = grid.empty() ? 0 : grid[0].size();
-    for (size_t j = 0; j < colCount; ++j) {
+    for (size_t j = 0; j < colCount; ++j)
+    {
         bool containsHash = false;
-        for (size_t i = 0; i < grid.size(); ++i) {
-            if (grid[i][j] == '#') {
+        for (size_t i = 0; i < grid.size(); ++i)
+        {
+            if (grid[i][j] == '#')
+            {
                 containsHash = true;
                 break;
             }
         }
-        if (!containsHash) {
+        if (!containsHash)
+        {
             emptyCols.push_back(j);
         }
     }
 
     return emptyCols;
+}
+
+std::string addStrings(const std::string &a, const std::string &b)
+{
+    std::string result;
+    int carry = 0;
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+
+    while (i >= 0 || j >= 0 || carry != 0)
+    {
+        int sum = carry;
+        if (i >= 0)
+        {
+            sum += a[i--] - '0';
+        }
+        if (j >= 0)
+        {
+            sum += b[j--] - '0';
+        }
+        carry = sum / 10;
+        result.push_back(sum % 10 + '0');
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
 }
 
 int main(int argc, char *argv[])
@@ -113,14 +158,19 @@ int main(int argc, char *argv[])
     std::vector<size_t> empty_columns = emptyColumns(grid);
 
     std::vector<Position> positions = findHashPositions(grid);
-    std::vector<long long> distnaces = findAllPairwiseDistances(positions, empty_rows, empty_columns, 2);
-    for (size_t i = 0; i < distnaces.size(); i++) {
-        std::cout << "Distance " << i << ": " << distnaces[i] << std::endl;
+    std::vector<long long> distnaces1 = findAllPairwiseDistances(positions, empty_rows, empty_columns, 2);
+    long long sum_of_distances1 = std::accumulate(distnaces1.begin(), distnaces1.end(), 0);
+    std::cout << "Sum of distances Part1: " << sum_of_distances1 << std::endl;
+
+    std::vector<long long> distnaces2 = findAllPairwiseDistances(positions, empty_rows, empty_columns, 1000000);
+
+    std::string sum_of_distances2 = "0";
+    for (const auto &distance : distnaces2)
+    {
+        sum_of_distances2 = addStrings(sum_of_distances2, std::to_string(distance));
     }
 
-    //long long sum_of_distances = std::accumulate(distnaces.begin(), distnaces.end(), 0);
-    
-    //std::cout << "Sum of distances: " << sum_of_distances << std::endl;
+    std::cout << "Sum of distances Part2: " << sum_of_distances2 << std::endl;
 
     return 0;
 }
